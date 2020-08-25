655b03c2e510c444c23a7ca9c3ff736e335d5a3e
Arthur <parabellum357@hotmail.com>
2020-08-25 13:31:21 +0500

    готово деление

--- tasks/task5/main.s
+++ tasks/task5/main.s
@@ -3,12 +3,14 @@ format_overflow_error: .asciz "overflow_error\n"
 format_decimal_number: .asciz "%d\n"
 format_wrong_operation: .asciz "wrong_operation\n"
 format_wrong_input_string: .asciz "wrong_input_string\n"
+format_divizion_by_zero: .asciz "divizion_by_zero\n"
 
 ptr_format_wrong_char: .word format_wrong_char
 ptr_format_overflow_error: .word format_overflow_error
 ptr_format_decimal_number: .word format_decimal_number
 ptr_format_wrong_operation: .word format_wrong_operation
 ptr_format_wrong_input_string: .word format_wrong_input_string
+ptr_format_divizion_by_zero: .word format_divizion_by_zero
 
 
 .text
@@ -38,8 +40,10 @@ my_atoi:
     movne r0, #1
     addeq r4, r4, #1 
     str r0, [fp, #-8] @сохраняем  "знак" числа
-
     mov r8, r4 @r8 - указатель на конец строки 
+
+@ идём до конца строки, заодно проверяем символы
+@ на корректность
 go_to_end_string:
     ldrb r0, [r8]
     cmp r0, #0 @ '\0'
@@ -49,7 +53,8 @@ go_to_end_string:
     beq my_atoi_wrong_char
     add r8, r8, #1
     b go_to_end_string
-    
+
+@for(p=1; end >= start; --end) num+=char*p; p*=10    
 my_atoi_main_loop:
     sub r8, r8, #1
     ldrb r5, [r8]
@@ -86,12 +91,88 @@ my_atoi_exit:
     bx lr
 
 div:
-    push {fp, lr} @ [sp-4]=lr, [sp-8]=fp, sp-=8
-	add fp, sp, #4 @ fp указывает на адрес возврата
+    push {fp, lr}
+	add fp, sp, #4
     sub sp, sp, #4
 
+    cmp r1, #0
+    beq div_divizion_by_zero
+    movgt r10, #1
+    movlt r10, #0
+    sublt r10, r10, #1
+    mullt r3, r1, r10
+    movlt r1, r3
 
+    cmp r0, #0
+    movgt r9, #1
+    movlt r9, #0
+    sublt r9, r9, #1
+    mullt r3, r0, r9
+    movlt r0, r3
 
+    mul r3, r9, r10
+    push {r3}
+
+    cmp r0, r1
+
+    movlt r0, #0
+    movlt r1, #0
+    blt div_exit
+
+    moveq r0, #0
+    moveq r1, #1
+    beq div_exit
+
+    @ r0 - 1 операнд, r1 - делитель, умноженный на r4*r6, r2 - 2 операнд,
+    @ r3 - частное, r5 - 10 в какой-то степени, r4 - предыдущая итерация r5,
+    @ r10 = 10, r7 - предыдущая итерация r1, r9 - tmp
+    mov r2, r1
+    mov r3, #0
+    mov r10, #10
+
+div_main_loop:
+    cmp r0, r2
+    blt div_main_loop_end
+    mov r1, r2
+    mov r4, #1
+    mov r5, #1
+    mov r6, #1
+
+div_find_y:
+    mul r9, r1, r5
+    cmp r0, r9
+    movle r7, r1
+    ble div_find_z
+    mov r1, r9
+    mov r4, r5
+    mul r9, r5, r10
+    mov r5, r9
+    b div_find_y
+
+div_find_z:
+    add r9, r1, r7
+    cmp r0, r9
+    ble div_found_z
+    mov r1, r9
+    add r6, r6, #1
+    b div_find_z
+
+div_found_z:
+    mul r9, r4, r6
+    add r3, r3, r9
+    sub r0, r0, r1
+    b div_main_loop
+
+div_divizion_by_zero:
+    mov r0, #1
+    b div_exit
+
+div_main_loop_end:
+    mov r0, #0
+    pop {r2}
+    mul r1, r2, r3
+
+div_exit:
     add sp, sp, #4
 	pop {fp, lr}
 	bx lr
@@ -109,7 +190,7 @@ from_stdin:
     bl gets
     mov r9, r0
     mov r0, #0
-    sub r4, fp, #12
+    sub r4, fp, #8
     str r9, [fp, #-8]
     str r0, [fp, #-12]
     str r0, [fp, #-16]
@@ -132,8 +213,7 @@ find_next_nonspace:
     bl isspace
     cmp r0, #0
     bne find_spaces
-    str r9, [r4]
-    sub r4, r4, #4
+    str r9, [r4, #-4]!
     b find_spaces
 
 find_spaces_end:
@@ -210,7 +290,8 @@ multiplication:
     b print_result
 divizion:
     bl div
-    mov r1, r0
+    cmp r0, #0
+    bne divizion_by_zero
     b print_result
 overflow_error:
     ldr r0, ptr_format_overflow_error
@@ -222,6 +303,10 @@ wrong_operation:
 
 wrong_input_string:
     ldr r0, ptr_format_wrong_input_string
+    b main_exit
+
+divizion_by_zero:
+    ldr r0, ptr_format_divizion_by_zero
     b main_exit
 
 print_result:
